a0087a753a749f03c0dc5ff24dde1a28fd7f212f
David Wenzel <dwenzel95@gmail.com>
2023-03-12 14:19:28 -0500

    board route actions index, create, destory

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -3,5 +3,20 @@ class BoardsController < ApplicationController
 
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-    
+    def index
+        boards = Board.all.order("title")
+        render json: boards, include: :reviews, status: :ok
+    end
+
+    def create
+        @current_user
+        board = Board.create!(board_params)
+        render json: board, status: :created
+    end
+
+    def destroy
+        board = Board.find(params[:id])
+        board.destroy
+        head :no_content
+    end
 end
