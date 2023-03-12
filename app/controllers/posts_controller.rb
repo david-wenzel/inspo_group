1a7e98a11d572b1792368a687bc2cfd39151a835
David Wenzel <dwenzel95@gmail.com>
2023-03-12 14:24:06 -0500

    add post crud actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,26 @@
 class PostsController < ApplicationController
+    def create
+        if params[:board_id]
+            game = Board.find(params[:board_id])
+            review = @current_user.reviews.create!(review_params)
+            render json: review, include: :user, status: :created
+
+        else
+            posts = Post.all
+            render json: posts, status: :created
+        end
+        
+    end
+
+    def update
+        post = @current_user.posts.find(params[:id])
+        post.update(post_params)
+        render json: post
+    end
+
+    def destroy
+        post = @current_user.posts.find(params[:id])
+        post.destroy
+        head :no_content
+    end
 end
