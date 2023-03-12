331979465465948507627c23a77f875b646b97eb
David Wenzel <dwenzel95@gmail.com>
2023-03-12 15:30:25 -0500

    add relationships, secure password macro, validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_many :posts
+    has_many :boards, through: :posts
+
+    has_secure_password
+
+    validates :username, presence: true, uniqueness: true
 end
