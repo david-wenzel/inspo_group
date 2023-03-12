232b530de2f1c1f5be8607914749c34fc426cc4d
David Wenzel <dwenzel95@gmail.com>
2023-03-12 15:28:34 -0500

    add board relationships

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,7 @@
 class Board < ApplicationRecord
+    has_many :posts, dependent: :destroy
+    has_many :users, through: :posts
+    scope :by_title, -> {order(title: :asc)}
+
+    validates :title, presence: true, uniqueness: true
 end
