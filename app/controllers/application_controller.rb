412133c7e4215ace1fbe2a1d3a9c934f4fcdc6e9
David Wenzel <dwenzel95@gmail.com>
2023-03-12 14:16:42 -0500

    add private methods in app controller autrhorize, review params, render errors from active recrod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,24 @@ class ApplicationController < ActionController::API
 
   before_action :authorize
 
+  def current_user
+    @current_user ||= session[:user_id] &&
+    User.find_by(id: session[:user_id])
+  end
+
+  private 
+  
+  def authorize
+    current_user
+    return render json: { errors: ["Not authorized"] }, status: :unauthorized unless session.include? :user_id
+  end
+
+  def review_params
+    params.permit(:title, :body, :rating, :game_id)
+  end
+
+  def render_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end
 
 end
