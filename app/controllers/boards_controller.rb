3110b3c60ad28b33f39a71887b58ea9f444dfca4
David Wenzel <dwenzel95@gmail.com>
2023-03-12 14:17:37 -0500

    add authorization before action to boards controller

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,2 +1,7 @@
 class BoardsController < ApplicationController
+    skip_before_action :authorize, only: [:index, :show]
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    
 end
